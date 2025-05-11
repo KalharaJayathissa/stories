1f0f78d021a5778bb5566945fe246ef2ece0bc9d
KalharaJayathissa <kalharajay@gmail.com>
2025-05-12 01:52:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function Types() {
     <Box sx={{ width: '100%', maxWidth: 500 ,margin: 'auto',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
       
       <Typography variant="h3" gutterBottom>
-        Go to the story here
+        Go to the story here.
       </Typography>
       <Button variant="contained" color="primary" onClick={() => navigate('/storypage')}>
         Send
