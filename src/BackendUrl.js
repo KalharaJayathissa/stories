0cc807fe88c9885e62854bae13b24e691c46e670
KalharaJayathissa <kalharajay@gmail.com>
2025-05-20 21:05:51 +0530

    Update BackendUrl.js

--- src/BackendUrl.js
+++ src/BackendUrl.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "https://storiesbedocker-production.up.railway.app"; // Change this if backend URL changes
+export const API_BASE_URL ="https://storiesb-egdhakdje3f8hsh9.canadacentral-01.azurewebsites.net/"; // Change this if backend URL changes
 
-
+// link of the railway hosted serv
+// https://storiesbedocker-production.up.railway.app
