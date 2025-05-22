7bee12b0389685b550f1e7aa09b15a07fd815737
KalharaJayathissa <kalharajay@gmail.com>
2025-05-22 15:35:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-// import Storypage from "./storypage";
+
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from "./AnimatedRouts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
