15aac4cc917de70b2b67d6886abca31e1683dec0
KalharaJayathissa <kalharajay@gmail.com>
2025-06-03 02:54:45 +0530

    del
    ja

--- src/BackendUrl.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL ="https://storiesb-egdhakdje3f8hsh9.canadacentral-01.azurewebsites.net/"; // Change this if backend URL changes
-
-// link of the railway hosted serv
-// https://storiesbedocker-production.up.railway.app
